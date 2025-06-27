cbd0d16531f9e296088a3d2a6db0f859dfb758aa
Vitor Furini <vfurini@lendingclub.com>
2025-06-26 22:51:00 -0300

    poc in java zip and unzip generated csv file

--- file-manager/src/CsvFileManager.java
+++ file-manager/src/CsvFileManager.java
@@ -1,5 +1,9 @@
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 
 public class CsvFileManager {
 
@@ -11,6 +15,12 @@ public class CsvFileManager {
 
         //read csv file
         readCsv(fileName);
+
+        //zip file
+        zipCsv(fileName);
+
+        //unzip file
+        unzipCsv(fileName.replace(".csv", ".zip"));
 
         //delte csv file
         //deleteFile(fileName);
@@ -63,6 +73,75 @@ public class CsvFileManager {
         } catch (IOException e) {
             System.err.println("Error reading CSV: " + e.getMessage());
         }
+    }
+    
+    private static void zipCsv(String fileName) {
+        System.out.println("\nZipping CSV file: " + fileName);
+        String zipFileName = fileName.replace(".csv", ".zip");
+        try (FileInputStream fis = new FileInputStream(fileName);
+             FileOutputStream fos = new FileOutputStream(zipFileName);
+             java.util.zip.ZipOutputStream zos = new java.util.zip.ZipOutputStream(fos)) {
+
+            zos.putNextEntry(new java.util.zip.ZipEntry(new File(fileName).getName()));
+
+            byte[] buffer = new byte[1024];
+            int length;
+            while ((length = fis.read(buffer)) >= 0) {
+                zos.write(buffer, 0, length);
+            }
+            zos.closeEntry();
+            System.out.println("CSV file zipped: " + zipFileName);
+
+        } catch (IOException e) {
+            System.err.println("Error zipping CSV: " + e.getMessage());
+        }
+    }
+
+    private static void unzipCsv(String replace) throws IOException {
+        File destDir = new File("file-manager/resources/unzipedFile");
+
+        byte[] buffer = new byte[1024];
+        ZipInputStream zis = new ZipInputStream(Files.newInputStream(Paths.get(replace)));
+        ZipEntry zipEntry = zis.getNextEntry();
+        while (zipEntry != null) {
+            File newFile = newFile(destDir, zipEntry);
+            if (zipEntry.isDirectory()) {
+                if (!newFile.isDirectory() && !newFile.mkdirs()) {
+                    throw new IOException("Failed to create directory " + newFile);
+                }
+            } else {
+                // fix for Windows-created archives
+                File parent = newFile.getParentFile();
+                if (!parent.isDirectory() && !parent.mkdirs()) {
+                    throw new IOException("Failed to create directory " + parent);
+                }
+
+                // write file content
+                FileOutputStream fos = new FileOutputStream(newFile);
+                int len;
+                while ((len = zis.read(buffer)) > 0) {
+                    fos.write(buffer, 0, len);
+                }
+                fos.close();
+            }
+            zipEntry = zis.getNextEntry();
+        }
+
+        zis.closeEntry();
+        zis.close();
+    }
+
+    public static File newFile(File destinationDir, ZipEntry zipEntry) throws IOException {
+        File destFile = new File(destinationDir, zipEntry.getName());
+
+        String destDirPath = destinationDir.getCanonicalPath();
+        String destFilePath = destFile.getCanonicalPath();
+
+        if (!destFilePath.startsWith(destDirPath + File.separator)) {
+            throw new IOException("Entry is outside of the target dir: " + zipEntry.getName());
+        }
+
+        return destFile;
     }
 
     private static void deleteFile(String fileName) {
