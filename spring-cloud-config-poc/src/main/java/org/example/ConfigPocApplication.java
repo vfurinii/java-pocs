e13a224f1ab14425638f7384501814ddd681dc25
Vitor Furini <vfurini@lendingclub.com>
2025-07-03 21:13:24 -0300

    poc with spring cloud config file last one

--- spring-cloud-config-poc/src/main/java/org/example/ConfigPocApplication.java
+++ spring-cloud-config-poc/src/main/java/org/example/ConfigPocApplication.java
@@ -1,9 +1,11 @@
 package org.example;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class ConfigPocApplication {
     public static void main(String[] args) {
         SpringApplication.run(ConfigPocApplication.class, args);
