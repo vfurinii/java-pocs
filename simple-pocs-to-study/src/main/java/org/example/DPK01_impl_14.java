eed78339c70a2473be116280f26d29f0badb7161
Vitor Furini <vfurini@lendingclub.com>
2025-09-10 18:59:26 -0300

    add DPK01_impl_14 with 2D Walk code #WIP

---
+++ simple-pocs-to-study/src/main/java/org/example/DPK01_impl_14.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class DPK01_impl_14 {
+
+    public static void main(String[] args) {
+        String[][] grid = {
+                {"Ryu", "E.Honda", "Blanka", "Guile", "Balrog", "Vega"},
+                {"Ken", "Chun Li", "Zangief", "Dhalsim", "Sagat", "M.Bison"}
+        };
+
+        int[] initialPosition = {0, 0};
+        String[] moves = {"right", "down", "left", "left", "left", "up"};
+
+        //List<String> result = streetFighterSelection(grid, initialPosition, moves);
+        System.out.println("result of moves");
+    }
+}
