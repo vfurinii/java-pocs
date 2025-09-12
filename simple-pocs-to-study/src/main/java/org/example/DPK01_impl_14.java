17a9706d9f0e6991595fcf6d23d00f1e3e9ae0b2
Vitor Furini <vfurini@lendingclub.com>
2025-09-11 23:15:58 -0300

    add DPK01_impl_14 with 2d walk and some unit tests

--- simple-pocs-to-study/src/main/java/org/example/DPK01_impl_14.java
+++ simple-pocs-to-study/src/main/java/org/example/DPK01_impl_14.java
@@ -1,17 +1,44 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class DPK01_impl_14 {
 
-    public static void main(String[] args) {
-        String[][] grid = {
-                {"Ryu", "E.Honda", "Blanka", "Guile", "Balrog", "Vega"},
-                {"Ken", "Chun Li", "Zangief", "Dhalsim", "Sagat", "M.Bison"}
-        };
+    public static List<String> move(String[][] grid, int[] startPos, String[] moves) {
+        int rows = grid.length;
+        int cols = grid[0].length;
 
-        int[] initialPosition = {0, 0};
-        String[] moves = {"right", "down", "left", "left", "left", "up"};
+        int row = startPos[0];
+        int col = startPos[1];
 
-        //List<String> result = streetFighterSelection(grid, initialPosition, moves);
-        System.out.println("result of moves");
+        List<String> beaten = new ArrayList<>();
+
+        for (String move : moves) {
+            switch (move.toLowerCase()) {
+                case "up":
+                    if (row > 0) {
+                        row--;
+                    }
+                    break;
+                case "down":
+                    if (row < rows - 1) {
+                        row++;
+                    }
+                    break;
+                case "left":
+                    col = (col - 1 + cols) % cols;
+                    break;
+                case "right":
+                    col = (col + 1) % cols;
+                    break;
+            }
+            if (!grid[row][col].isEmpty()) {
+                beaten.add(grid[row][col]);
+                grid[row][col] = "";
+            }
+        }
+
+        return beaten;
     }
 }
