145931505956fbc12caf613ffd42e3fefa71b65e
Vitor Furini <vitormfurini@gmail.com>
2025-12-23 21:17:08 -0300

    Create DPK01_impl_5.java
    Create DPK01_impl_5.java

---
+++ simple-pocs-to-study/src/main/java/org/example/DPK01/DPK01_impl_5.java
@@ -0,0 +1,14 @@
+public class DPK01_impl_5 {
+
+    public static String revert(String input){
+        if (null == input) return null;
+
+        StringBuilder result = new StringBuilder();
+
+        for (int i = input.length()-1; i >=0 ; i--) {
+            result.append(input.toCharArray()[i]);
+        }
+
+        return result.toString();
+    }
+}
