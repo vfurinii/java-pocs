00c899681cce05ee2570dc96667e92aefd2bb898
Vitor Furini <vitormfurini@gmail.com>
2026-03-27 22:34:19 -0300

    added DPK03_impl_2 code

--- simple-pocs-to-study/src/main/java/org/example/DPK03/DPK03_impl_2.java
+++ simple-pocs-to-study/src/main/java/org/example/DPK03/DPK03_impl_2.java
@@ -1,11 +1,15 @@
 package org.example.DPK03;
 
-import java.util.HashMap;
 import java.util.Map;
 
-//Create a function that perform a lookup in a map for a given key you hould have id, name.
-//lookup(1) -> "John"
 public class DPK03_impl_2 {
 
-
+    public static String lookup(Map<Integer, String> idNameMap, Integer id) {
+        for (Map.Entry<Integer, String> entry : idNameMap.entrySet()) {
+            if (entry.getKey().equals(id)) {
+                return entry.getValue();
+            }
+        }
+        return "Key not found";
+    }
 }
