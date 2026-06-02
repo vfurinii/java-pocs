324bdc08660d590ae4704794c6352078720f14f5
Vitor Furini <vitormfurini@gmail.com>
2026-06-01 21:15:38 -0300

    added some updates to coverage step 3

--- simple-pocs-to-study/src/main/java/org/example/DPK16/Game.java
+++ simple-pocs-to-study/src/main/java/org/example/DPK16/Game.java
@@ -4,16 +4,16 @@ import lombok.Getter;
 import lombok.Setter;
 
 /*
-The game should start with 10 mosquitos, 1 exterminator.
-The game should have a internal matrix of 100x100.
-Every 1s the mosquito should move to a random position.
-Every 1s the exterminator should move to a specific position.
-If the mosquito and the exterminator are in the same position, the mosquito should die. ok
-If the mosquito moves 5 times without being killed, it should reproduce if there is a mosquito nearby.
-The game should have a method that can return the number of mosquitos killed.
-The game should have a method that can return the number of mosquitos alive.
-The mosquito can walk in any direction (up, down, left, right, diagonals).
-The exterminator can walk in one direction, it should swap the metrixs.
+1.The game should start with 10 mosquitos, 1 exterminator. ok
+2.The game should have a internal matrix of 100x100. ok
+3.Every 1s the mosquito should move to a random position.
+4.Every 1s the exterminator should move to a specific position.
+5.If the mosquito and the exterminator are in the same position, the mosquito should die. ok
+6.If the mosquito moves 5 times without being killed, it should reproduce if there is a mosquito nearby.
+7.The game should have a method that can return the number of mosquitos killed.
+8.The game should have a method that can return the number of mosquitos alive.
+9.The mosquito can walk in any direction (up, down, left, right, diagonals).
+10.The exterminator can walk in one direction, it should swap the metrixs.
 The exterminator can walk from the bottown left corner to the top right
 corner them from the botton to the top them return on the same route.
 * */
@@ -28,12 +28,19 @@ public class Game {
 
     private int[][] matrix =  new int[100][100];
 
+    @Setter
+    @Getter
+    private Mosquito[] mosquitos = new Mosquito[10];
+
     public Game(Exterminator exterminator) {
         this.exterminator = exterminator != null ? exterminator : new Exterminator();
 
-        for (int i = 0; i < mosquito.length; i++) {
-            mosquito[i] = new Mosquito();
+        for (int i = 0; i < mosquitos.length; i++) {
+            mosquitos[i] = new Mosquito();
         }
+
+        initializeMosquitoPositions();
+        initializeExterminatorPositions();
     }
 
     private void startGame(){
